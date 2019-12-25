9e466963cc5bf4c3251a60136f423bfa6e5e8c17
Rani <ranisans@yahoo.com>
2019-12-25 20:45:37 +0300

    feat: add right bar and preview window

--- src/js/gui/main/Main.js
+++ src/js/gui/main/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import LeftBar from './leftBar/LeftBar';
+import RightBar from './rightBar/RightBar';
 
 const Main = () => (
   <main className="main_block">
     <LeftBar />
+    <RightBar />
   </main>
 );
 
---
+++ src/js/gui/main/rightBar/RightBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PreviewWindow from './preview/PreviewWindow';
+
+const RightBar = () => (
+  <div className="right_bar">
+    <PreviewWindow />
+  </div>
+);
+
+export default RightBar;
