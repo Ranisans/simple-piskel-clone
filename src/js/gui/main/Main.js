70c32c8df08234bfa4e681259fcc2a305194a881
Rani <ranisans@yahoo.com>
2019-12-27 12:37:11 +0300

    feat: add canvas, resize canvas to window size

--- src/js/gui/main/Main.js
+++ src/js/gui/main/Main.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import LeftBar from './leftBar/LeftBar';
 import RightBar from './rightBar/RightBar';
+import Canvas from './canvas/Canvas';
 
-const Main = () => (
-  <main className="main_block">
-    <LeftBar />
-    <RightBar />
-  </main>
-);
+const Main = () => {
+  let mainHeight = 0;
+  let canvasContainer;
+  let mainCanvas;
+
+  const resizeWindow = () => {
+    if (mainHeight === 0) {
+      canvasContainer = document.querySelector('.main_canvas_container');
+      mainCanvas = document.querySelector('.main_canvas');
+      mainHeight = canvasContainer.clientHeight;
+    }
+    const containerWidth = canvasContainer.clientWidth;
+    const newSize = Math.floor(Math.min(mainHeight, containerWidth * 0.9));
+    mainCanvas.style.height = `${newSize}px`;
+    mainCanvas.style.width = `${newSize}px`;
+    mainCanvas.style.marginTop = `${(mainHeight - newSize) / 2}px`;
+    mainCanvas.style.marginLeft = `${(containerWidth - newSize) / 2}px`;
+  };
+
+  useEffect(() => {
+    resizeWindow();
+  }, []);
+
+  window.addEventListener('resize', resizeWindow);
+
+  return (
+    <main className="main_block">
+      <LeftBar />
+      <Canvas />
+      <RightBar />
+    </main>
+  );
+};
 
 export default Main;
---
+++ src/js/gui/main/canvas/Canvas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Canvas = () => (
+  <div className="main_canvas_container">
+    <canvas className='main_canvas'></canvas>
+  </div>
+);
+
+export default Canvas;
