02f67d5fbcfad322038a64116e96a5bf29b07a8f
Rani <ranisans@yahoo.com>
2019-12-30 22:13:55 +0300

    feat: add canvas logic call

--- src/js/gui/main/Main.js
+++ src/js/gui/main/Main.js
@@ -1,30 +1,56 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import LeftBar from './leftBar/LeftBar';
 import RightBar from './rightBar/RightBar';
 import Canvas from './canvas/Canvas';
+import { changeCanvasBoxSize, mainCanvasClass } from '../../actions/canvasAction';
+import CanvasLogic from './canvas/CanvasLogic';
 
 const Main = () => {
   let mainHeight = 0;
-  let canvasContainer;
-  let mainCanvas;
+  let mainCanvasBlock;
+  let mainCanvasContainer;
+  const dispatch = useDispatch();
 
   const resizeWindow = () => {
     if (mainHeight === 0) {
-      canvasContainer = document.querySelector('.main_canvas_block');
-      mainCanvas = document.querySelector('.main_canvas_container');
-      mainHeight = canvasContainer.clientHeight;
+      mainCanvasBlock = document.querySelector('.main_canvas_block');
+      mainCanvasContainer = document.querySelector('.main_canvas_container');
+      mainHeight = mainCanvasBlock.clientHeight;
     }
-    const containerWidth = canvasContainer.clientWidth;
+    const containerWidth = mainCanvasBlock.clientWidth;
     const newSize = Math.floor(Math.min(mainHeight, containerWidth * 0.9));
-    mainCanvas.style.height = `${newSize}px`;
-    mainCanvas.style.width = `${newSize}px`;
-    mainCanvas.style.marginTop = `${(mainHeight - newSize) / 2}px`;
-    mainCanvas.style.marginLeft = `${(containerWidth - newSize) / 2}px`;
+    mainCanvasContainer.style.height = `${newSize}px`;
+    mainCanvasContainer.style.width = `${newSize}px`;
+    mainCanvasContainer.style.marginTop = `${(mainHeight - newSize) / 2}px`;
+    mainCanvasContainer.style.marginLeft = `${(containerWidth - newSize) / 2}px`;
+    dispatch(changeCanvasBoxSize({ size: newSize }));
   };
 
   useEffect(() => {
     resizeWindow();
+    const canvas = document.querySelector(`.${mainCanvasClass}`);
+    const canvasLogic = new CanvasLogic();
+
+    canvas.addEventListener('changeCanvasSize', () => {
+      canvasLogic.settingCanvasSize();
+    });
+
+    canvas.addEventListener('changePenSize', () => {
+      canvasLogic.settingPixelSize();
+    });
+
+    canvas.addEventListener('changeTool', () => {
+      canvasLogic.settingTool();
+    });
+
+    canvas.addEventListener('reset', () => {
+      canvasLogic.resetAll();
+    });
+
+    canvasLogic.settingPixelSize();
+    canvasLogic.settingCanvasSize();
   }, []);
 
   window.addEventListener('resize', resizeWindow);
