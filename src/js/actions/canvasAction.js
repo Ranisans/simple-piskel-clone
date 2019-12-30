797671ce35b20f8d30c2a3a15e468586c34351b4
Rani <ranisans@yahoo.com>
2019-12-30 22:07:52 +0300

    feat: add redux state for canvas box size

--- src/js/actions/canvasAction.js
+++ src/js/actions/canvasAction.js
@@ -3,10 +3,18 @@ export const mainCanvasClass = 'main_canvas';
 export const canvasSizes = [32, 64, 128];
 
 export const CHANGE_CANVAS_SIZE = 'CHANGE_CANVAS_SIZE';
+export const CHANGE_CANVAS_BOX_SIZE = 'CHANGE_CANVAS_BOX_SIZE';
 
 export const changeCanvasSize = ({ size }) => (
   {
     type: CHANGE_CANVAS_SIZE,
+    size,
+  }
+);
+
+export const changeCanvasBoxSize = ({ size }) => (
+  {
+    type: CHANGE_CANVAS_BOX_SIZE,
     size,
   }
 );
--- src/js/reducers/canvasReducer.js
+++ src/js/reducers/canvasReducer.js
@@ -1,7 +1,8 @@
-import { canvasSizes, CHANGE_CANVAS_SIZE } from '../actions/canvasAction';
+import { canvasSizes, CHANGE_CANVAS_SIZE, CHANGE_CANVAS_BOX_SIZE } from '../actions/canvasAction';
 
 export const initialState = {
   size: canvasSizes[0],
+  boxSize: 0,
 };
 
 export const canvasReducer = (state = initialState, action) => {
@@ -10,6 +11,11 @@ export const canvasReducer = (state = initialState, action) => {
       return {
         ...state,
         size: action.size,
+      };
+    case CHANGE_CANVAS_BOX_SIZE:
+      return {
+        ...state,
+        boxSize: action.size,
       };
     default:
       return state;
