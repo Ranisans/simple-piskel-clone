6fe2498b57464edd8a77b631fff7d1eb91a620a1
Rani <ranisans@yahoo.com>
2020-01-02 00:18:44 +0300

    feat: add creating a new frame when press button or if have no frame add unique id for every frame

--- src/gui/main/leftBar/frame/FrameBlock.js
+++ src/gui/main/leftBar/frame/FrameBlock.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import uuid from 'uuid-random';
 
 import AddFrameButton from './AddFrameButton';
 import Frame from './Fame';
+import { addFrame } from '../../../../actions/frameAction';
 
 const FrameBlock = () => {
-  const addFrame = () => {
-    console.log('Add Frame');
+  const dispatch = useDispatch();
+  const frameList = useSelector((state) => state.frameList);
+
+  const addCleanFrame = () => {
+    dispatch(addFrame({ frameId: uuid() }));
   };
+
+  if (frameList.frames.length === 0) {
+    addCleanFrame();
+  }
+
   return (
     <div className="frame_block">
       <ul className="preview_list">
-        <Frame />
+        {frameList.frames.map((frameId) => <Frame frameId={frameId} key={frameId} />)}
       </ul>
-      <AddFrameButton callback={addFrame} />
+      <AddFrameButton callback={addCleanFrame} />
     </div>
   );
 };
