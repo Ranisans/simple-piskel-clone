dcf3825f27124c3b70a833df6de13cb38a1edcec
Rani <ranisans@yahoo.com>
2020-01-05 12:03:30 +0300

    feat: add resolution changer and lint it to canvas state

---
+++ src/gui/main/rightBar/Resolution.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { changeCanvasSize } from '../../../actions/canvasAction';
+
+
+const Resolution = () => {
+  const dispatch = useDispatch();
+
+  const canvasResolutions = [32, 64, 128];
+  const [resolution, setResolution] = useState(canvasResolutions[0]);
+
+  const menuClass = 'resolution_menu';
+
+  const resolutionHandle = (e) => {
+    const { value } = e.target;
+    setResolution(value);
+    dispatch(changeCanvasSize({ size: value }));
+  };
+
+  return (
+    <div>
+      <label htmlFor={menuClass} className={`${menuClass}_label`}>Canvas Resolution</label>
+      <select
+        id={menuClass}
+        name={menuClass}
+        className={menuClass}
+        onChange={resolutionHandle}
+        value={resolution}
+      >
+        {canvasResolutions.map(
+          (item, key) => <option
+            key={key}
+            className={`${menuClass}-item`}
+            value={item}
+          > {`${item}x${item}`}</option>,
+        )}
+      </select>
+    </div >
+  );
+};
+
+export default Resolution;
--- src/gui/main/rightBar/RightBar.js
+++ src/gui/main/rightBar/RightBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import PreviewWindow from './preview/PreviewWindow';
+import Resolution from './Resolution';
 
 const RightBar = () => (
   <div className="right_bar">
     <PreviewWindow />
+    <Resolution />
   </div>
 );
 
