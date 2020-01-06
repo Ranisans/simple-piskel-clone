8ca28ee35fa0ec9e9886a62032cf250ef27d012e
Rani <ranisans@yahoo.com>
2020-01-06 16:56:08 +0300

    feat: add animation preview, fullscreen

--- src/gui/main/rightBar/preview/PreviewLogic.js
+++ src/gui/main/rightBar/preview/PreviewLogic.js
@@ -1,9 +1,14 @@
 class PreviewLogic {
+  constructor() {
+    this.second = 1000;
+  }
+
   initialize(previewId, previewBoxSize) {
     this.canvas = document.querySelector(`#${previewId}`);
     this.canvas.width = previewBoxSize;
     this.canvas.height = previewBoxSize;
     this.context = this.canvas.getContext('2d');
+    this.container = document.querySelector('.preview_canvas_container');
   }
 
   setSize(size) {
@@ -14,7 +19,47 @@ class PreviewLogic {
   }
 
   setFullscreen() {
-    this.canvas.requestFullscreen();
+    this.container.requestFullscreen();
+  }
+
+  setFps(fps) {
+    this.fps = fps;
+    clearInterval(this.intervalId);
+    this.startAnimate();
+  }
+
+  setFrames(frames) {
+    this.frames = frames;
+    clearInterval(this.intervalId);
+    this.startAnimate();
+  }
+
+  setFramesQueue(framesQueue) {
+    this.framesQueue = framesQueue;
+    this.animatePosition = 0;
+    clearInterval(this.intervalId);
+    this.startAnimate();
+  }
+
+  startAnimate() {
+    const animate = this._animate.bind(this);
+    this.intervalId = setInterval(animate, this.second / this.fps);
+  }
+
+  _animate() {
+    if (!this.frames || !this.framesQueue) { return; }
+    const { imageData } = this.frames[this.framesQueue[this.animatePosition]];
+    if (imageData) {
+      this.context.putImageData(imageData, 0, 0);
+    } else {
+      this.context.fillStyle = 'rgba(0,0,0,0)';
+      this.context.clearRect(0, 0, this.canvasSize, this.canvasSize);
+    }
+    if (this.animatePosition + 1 >= this.framesQueue.length) {
+      this.animatePosition = 0;
+    } else {
+      this.animatePosition += 1;
+    }
   }
 }
 
--- src/gui/main/rightBar/preview/PreviewWindow.js
+++ src/gui/main/rightBar/preview/PreviewWindow.js
@@ -7,19 +7,34 @@ const PreviewWindow = () => {
   const previewSize = 200;
   const previewId = 'preview_window';
   const [previewLogic] = useState(new PreviewLogic());
-  const [fps, setFps] = useState(1);
+  const [fps, setFps] = useState(5);
 
   const canvasState = useSelector((state) => state.canvas);
+  const frameState = useSelector((state) => state.frame);
+  const frameListState = useSelector((state) => state.frameList);
 
   // didMount
   useEffect(() => {
     previewLogic.initialize(previewId, previewSize);
+    previewLogic.startAnimate();
   }, []);
 
   // didUpdate canvas size
   useEffect(() => {
     previewLogic.setSize(canvasState.size);
   }, [canvasState.size]);
+
+  useEffect(() => {
+    previewLogic.setFps(fps);
+  }, [fps]);
+
+  useEffect(() => {
+    previewLogic.setFrames(frameState);
+  }, [frameState]);
+
+  useEffect(() => {
+    previewLogic.setFramesQueue(frameListState.frames);
+  }, [frameListState]);
 
   return (
     <div className="preview_container">
@@ -34,7 +49,7 @@ const PreviewWindow = () => {
       </div>
       <div className="preview_actions">
         <div className="preview_actions-fullscreen" onClick={() => { previewLogic.setFullscreen(); }}></div>
-        <p>{`${fps} fps`}</p>
+        <p className="preview_actions-fps_label">{`${fps} fps`}</p>
         <input
           type="range" name="fps" id="fps" className="preview_actions-fps"
           min="1" max="24" step="1"
